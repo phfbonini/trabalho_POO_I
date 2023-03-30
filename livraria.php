b4edfea3efba7dafc1d03484129bd8195798d14e
phfbonini <phf_apps@outlook.com>
2023-03-30 10:27:26 -0300

    EXERCICIO 2 :  Adicione métodos getter e setter para cada atributo.

--- livraria.php
+++ livraria.php
@@ -6,5 +6,53 @@ class Livro {
     private $preco;
     private $desconto;
     private $estoque;
+
+    public function getTitulo() {
+        return $this->titulo;
+    }
+
+    public function setTitulo($titulo) {
+        $this->titulo = $titulo;
+    }
+
+    public function getAutor() {
+        return $this->autor;
+    }
+
+    public function setAutor($autor) {
+        $this->autor = $autor;
+    }
+
+    public function getEditora() {
+        return $this->editora;
+    }
+
+    public function setEditora($editora) {
+        $this->editora = $editora;
+    }
+
+    public function getPreco() {
+        return $this->preco;
+    }
+
+    public function setPreco($preco) {
+        $this->preco = $preco;
+    }
+
+    public function getDesconto() {
+        return $this->desconto;
+    }
+
+    public function setDesconto($desconto) {
+        $this->desconto = $desconto;
+    }
+
+    public function getEstoque() {
+        return $this->estoque;
+    }
+
+    public function setEstoque($estoque) {
+        $this->estoque = $estoque;
+    }
 }
 ?>
